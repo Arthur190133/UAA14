7c09d8c1487e836af750f4c59aa9b0e271db625c
Ryckbosch Arthur <190133@CCM-Asty-Moulin.local>
2022-09-13 14:28:00 +0200

    Premier POO

---
+++ POO/ChienPOO/ChienPOO/Chien.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChienPOO
+{
+    class Chien
+    {
+        private string Nom;
+        private string Race;
+        private int Age;
+
+        public Chien(string NomChien, string RaceChien, int AgeChien)
+        {
+            Nom = NomChien;
+            Race = RaceChien;
+            Age = AgeChien;
+        }
+
+        public void AfficheCaracteristiques()
+        {
+            string Affiche = "";
+
+            Affiche += "Nom : " + Nom;
+            Affiche += " - Age : " + Age;
+            Affiche += " - Race : " + Race.ToString();
+
+            Console.WriteLine(Affiche);
+        }
+    }
+}
---
+++ POO/ChienPOO/ChienPOO/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChienPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Chien[] chiens = new Chien[3];
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Entrer le nom du chien");
+                string nom = Console.ReadLine();
+                Console.WriteLine("Entrer la race du chien");
+                string race = Console.ReadLine();
+                Console.WriteLine("Entrer l'age du chien");
+                int age = int.Parse(Console.ReadLine());
+
+                chiens[i] = new Chien(nom, race, age);
+            }
+            for(int i = 0; i < 3; i++)
+            {
+                chiens[i].AfficheCaracteristiques();
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
