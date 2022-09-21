112a015e10b357a293c08b236b31b01b0161d0b5
Arthur190133 <190133@site.asty-moulin.be>
2022-09-21 20:14:33 +0200

    fin act1 ex2

--- WPF/ACT1_Layouts/ACT1_Layouts/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ WPF/ACT1_Layouts/ACT1_Layouts/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -45,7 +45,7 @@ namespace ACT1_Layouts {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace ACT1_Layouts {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public static void Main() {
             ACT1_Layouts.App app = new ACT1_Layouts.App();
             app.InitializeComponent();
