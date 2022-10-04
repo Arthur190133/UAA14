2d59a0b41a49e80086accb7922c89eef4150e6b9
Ryckbosch Arthur <190133@CCM-Asty-Moulin.local>
2022-10-04 14:02:38 +0200

    Ajouet interro 1

---
+++ POO/I1P622_Arthur/I1P622_Arthur/Feu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace I1P622_Arthur
+{
+    class Feu
+    {
+        private string feuId;
+        private int feuCouleur;
+        private bool feuEtat;
+
+        public Feu(string id, int couleur, bool etat)
+        {
+            feuId = id;
+            feuCouleur = couleur;
+            feuEtat = etat;
+        }
+        public void changerCouleur(int newCouleur)
+        {
+            feuCouleur = newCouleur;
+        }
+
+        public string switchFeuEtat()
+        {
+            feuEtat = !feuEtat;
+            if(feuEtat) { return "allumé"; };
+            return "éteint";
+        }
+
+        public string getFeuId()
+        {
+            return feuId;
+        }
+
+        public string getFeuCouleur()
+        {
+            string couleur = null;
+            switch (feuCouleur)
+            {
+                case 0:
+                    couleur = "rouge";
+                    break;
+
+                case 1:
+                    couleur = "orange";
+                    break;
+
+                case 2:
+                    couleur = "vert";
+                    break;
+            }
+            return couleur;
+        }
+    }
+
+
+}
---
+++ POO/I1P622_Arthur/I1P622_Arthur/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace I1P622_Arthur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Feu[] feu = new Feu[2];
+            feu[1] = new Feu("67", 0, false);
+            feu[0] = new Feu("89", 0, false);
+
+            lancerFeu(feu[0]);
+            lancerFeu(feu[1]);
+
+            Console.WriteLine("Etat des feux \n----------------- \n" + getEtatFeu(feu) + "\n\n\n");
+            Console.WriteLine("Changement d'état \n----------------- \n");
+            for (int i = 0; i < 4; i++)
+            {
+                changerEtatFeu(feu[0]);
+                System.Threading.Thread.Sleep(100);
+            }
+            changerEtatFeu(feu[0]);
+            Console.WriteLine(" \n\n\nfeu clignotant \n------------\n");
+            
+
+            for(int i = 0; i < 8; i++)
+            {
+                swtichEtatFeu(feu[1]);
+                System.Threading.Thread.Sleep(500);
+            }
+
+            
+            Console.ReadLine();
+            
+        }
+
+        static void lancerFeu(Feu feu)
+        {
+            feu.switchFeuEtat();
+            feu.changerCouleur(1);
+        }
+
+        static string getEtatFeu(Feu[] feu)
+        {
+            string etat = null;
+            for(int i = 0; i < feu.Length; i++)
+            {
+                etat += "Le feu de signalisation " + feu[i].getFeuId() + " est " + feu[i].getFeuCouleur() + "\n";
+            }
+            return etat;
+        }
+
+        static void changerEtatFeu(Feu feu)
+        {
+            Random random = new Random();
+            feu.changerCouleur(random.Next(0, 3));
+
+            Console.WriteLine("Le feu de signalisation " + feu.getFeuId() + " est " + feu.getFeuCouleur());
+        }
+
+        static void swtichEtatFeu(Feu feu)
+        {
+            string etat = feu.switchFeuEtat();
+            Console.WriteLine(feu.getFeuId() + " est " + etat);
+        }
+    }
+}
