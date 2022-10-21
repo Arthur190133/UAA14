f154c141ef97df20c39fe45bfae3c8b879f92690
Ryckbosch Arthur <190133@CCM-Asty-Moulin.local>
2022-10-21 13:44:45 +0200

    fin matchinggame dynamique

--- WPF/ACT4_MatchingGame/ACT4_MatchingGame/MainWindow.xaml.cs
+++ WPF/ACT4_MatchingGame/ACT4_MatchingGame/MainWindow.xaml.cs
@@ -28,6 +28,8 @@ namespace ACT4_MatchingGame
         }
         private void InitializeInterface()
         {
+            FontSize = 36;
+
             for (int i = 0; i < 4; i++)
             {
                 ColumnDefinition Column = new ColumnDefinition();
@@ -42,18 +44,19 @@ namespace ACT4_MatchingGame
                 {
                     cartes[i, j] = new TextBlock();
                     cartes[i, j].Text = "?";
-                    cartes[i, j].FontSize = 25;
-                    SetTextBlock(cartes[i, j], null);
+                    cartes[i, j].MouseDown += new MouseButtonEventHandler(SetTextBlock);
                     Grid.SetColumn(cartes[i, j], j);
                     Grid.SetRow(cartes[i, j], i);
                     grid.Children.Add(cartes[i,j]);
+                    cartes[i, j].HorizontalAlignment = HorizontalAlignment.Center;
+                    cartes[i, j].VerticalAlignment = VerticalAlignment.Center;
                 }
             }
         }
 
         private void SetTextBlock(object sender, MouseButtonEventArgs e)
         {
-            
+            ((TextBlock)sender).Text = "X";
         }
     }
 }
