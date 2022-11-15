9bd7139dd564b5aca828dbbd861c00987c4f3aa8
Ryckbosch Arthur <190133@CCM-Asty-Moulin.local>
2022-11-15 14:22:31 +0100

    Ajout act4

---
+++ POO/ACT4_Cercle/ACT4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ACT4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cercle cercle = new Cercle(); 
+
+            Console.WriteLine("Bienvenue dans ce programme sur le cercle");
+            do
+            {
+                Console.WriteLine("Tapez un rayon pour votre cercle");
+                cercle.Rayon = double.Parse(Console.ReadLine());
+                cercle.Caracteristique();
+                Console.WriteLine("Un autre cercle ? (Tapez sur espace)");
+                Console.ReadLine();
+            } while (0 == 0);
+
+        }   
+    }
+}
---
+++ POO/ACT4_Personne/ACT4_Personne/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace ACT4_Personne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne Personne1;
+            Personne Personne2;
+
+            Console.WriteLine("Bienvenue dans notre gestionnaire de porte monnaie !");
+
+            do
+            {
+                Console.WriteLine("Premiere personne, quel est votre nom ?");
+                string nom = Console.ReadLine();
+
+                
+                int cash = 0;
+                string Cash;
+                do
+                {
+                    Console.WriteLine("Tapez une somme d'argent en euros");
+                    Cash = Console.ReadLine();
+                } while (!int.TryParse(Cash, out cash));
+
+                Personne1 = new Personne(nom, cash);
+
+                Console.WriteLine("deuxieme personne, quel est votre nom ?");
+                nom = Console.ReadLine();
+
+                do
+                {
+                    Console.WriteLine("Tapez une somme d'argent en euros");
+                    Cash = Console.ReadLine();
+                } while (!int.TryParse(Cash, out cash));
+
+                Personne2 = new Personne(nom, cash);
+
+                Console.WriteLine(Personne1.Nom + " combien voulez vous donner à " + Personne2.Nom);
+                int euros;
+                string value;
+                do
+                {
+                    Console.WriteLine("Tapez une somme d'argent en euros");
+                    value = Console.ReadLine();
+                } while (!int.TryParse(value, out euros));
+
+                if(Personne1.ALaThune(euros))
+                {
+                    Personne1.Cash -= euros;
+                    Personne2.Cash += euros;
+                    Console.WriteLine("Ajout effectué !");
+                }
+                else
+                {
+                    Console.WriteLine(Personne1.Nom + " vous n'avez pas asser d'argent pour cettre transaction !");
+                }
+
+                Personne1.Afficher();
+                Personne2.Afficher();
+
+
+
+
+                Console.WriteLine(Personne2.Nom + " combien voulez vous donner à " + Personne1.Nom);
+                do
+                {
+                    Console.WriteLine("Tapez une somme d'argent en euros");
+                    value = Console.ReadLine();
+                } while (!int.TryParse(value, out euros));
+
+                if (Personne2.ALaThune(euros))
+                {
+                    Personne2.Cash -= euros;
+                    Personne1.Cash += euros;
+                    Console.WriteLine("Ajout effectué !");
+                }
+                else
+                {
+                    Console.WriteLine(Personne2.Nom + " vous n'avez pas asser d'argent pour cettre transaction !");
+                }
+
+                Personne1.Afficher();
+                Personne2.Afficher();
+
+                Console.WriteLine("Tapez sur espace pour recommencer avec deux autres personnes");
+                Console.ReadLine();
+
+
+            } while (0 == 0);
+        }
+    }
+}
