a6c44e76007ef5469e74f2d2688d24dbfd7f1cea
Ryckbosch Arthur <190133@CCM-Asty-Moulin.local>
2022-11-22 14:15:02 +0100

    ajout act 5

---
+++ POO/ACT_5Gumballs/ACT_5Gumballs/GumballMachine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACT_5Gumballs
+{
+    class GumballMachine
+    {
+        private int _gumballs;
+        private int _price;
+
+        public int Price
+        {
+            get
+            {
+                return _price;
+            }
+             
+        }
+
+        public GumballMachine(int gumballs, int price)
+        {
+            _gumballs = gumballs;
+            _price = price;
+        }
+
+        public string DispenseOneGumball(int coinsInserted)
+      {
+            if (coinsInserted >= _price)
+          {
+                _gumballs -= 1;
+                return "Voici votre gomme !";
+            }
+            else
+            {
+                return "Pas assez de pièces !";
+            }
+        }
+    }
+}
