06cbd58abb1fd0b149d969376c98afc41ee54951
Ryckbosch Arthur <190133@CCM-Asty-Moulin.local>
2022-12-09 14:20:12 +0100

    ajout cal.bin

--- WPF_POO/CalculetteBinairePro/CalculetteBinaire/MainWindow.xaml.cs
+++ WPF_POO/CalculetteBinairePro/CalculetteBinaire/MainWindow.xaml.cs
@@ -69,11 +69,16 @@ namespace CalculetteBinaire
                 switch (prop)
                 {
                     case "+":
-                        valeur = Addition(FirstNbr.ToString(), SecondNbr.ToString());
+                        //valeur = Addition(FirstNbr.ToString(), SecondNbr.ToString());
+                        valeur =  binaireToString(Add(FillArray(FirstNbr.ToString()), FillArray(SecondNbr.ToString())));
+                        //arr(FillArray(SecondNbr.ToString()));
                         break;
 
                     case "-":
-                        valeur = Soustraction(FirstNbr.ToString(), SecondNbr.ToString());
+                        //valeur = Soustraction(FirstNbr.ToString(), SecondNbr.ToString());
+                        ushort[] bin = new ushort[8];
+                        Sous(FillArray(FirstNbr.ToString()), FillArray(SecondNbr.ToString()),ref bin);
+                        valeur = binaireToString(bin);
                         break;
 
                     case "x":
@@ -93,6 +98,18 @@ namespace CalculetteBinaire
             }
         }
 
+        private string binaireToString(ushort[] binaire)
+        {
+            string result = "";
+
+            for(int i = 0; i < binaire.Length; i++)
+            {
+                result += binaire[i];
+            }
+
+            return result;
+        }
+
         private void Clear(object sender, RoutedEventArgs e)
         {
             Afficheur.Text = "";
@@ -104,6 +121,100 @@ namespace CalculetteBinaire
             string sum = Convert.ToString(Convert.ToInt32(nbr1, 2) + Convert.ToInt32(nbr2, 2), 2).PadLeft(4, '0');
 
             return sum;
+        }
+
+        private ushort[] Add(ushort[] nbr1, ushort[] nbr2)
+        {
+            bool ok = false;
+            int report = 0;
+
+            ushort[] arrayRes = new ushort[8];
+
+            for(int i = 7; i > 0; i--)
+            {
+                int res = nbr1[i] + nbr2[i] + report;
+                if(res / 2 == 0)
+                {
+                    report = 0;
+                }
+                else
+                {
+                    report = 1;
+                }
+
+                if(res == 1)
+                {
+                    arrayRes[i] = 1;
+                }
+                else
+                {
+                    if(res % 2 == 1)
+                    {
+                        arrayRes[i] = 1;
+                    }
+                    else
+                    {
+                        arrayRes[i] = 0;
+                    }
+                }
+            }
+
+            if(report == 1)
+            {
+                ok = false;
+            }
+
+            return arrayRes;
+        }
+
+        private bool Sous(ushort[] nbr1, ushort[] nbr2,ref ushort[]binaire)
+        {
+            int[] arrayTemp = new int[8];
+            ushort[] arrayRes = new ushort[8];
+            bool ok = true;
+
+            for(int i = 0; i < 7; i++)
+            {
+                arrayTemp[i] = (ushort)(nbr1[i] - nbr2[i]);
+            }
+
+            for(int i = 7; i > 1; i--)
+            {
+                if(arrayTemp[i] == -1)
+                {
+                    nbr2[i - 1]++;
+                    nbr1[i] += 2;
+                }
+                if(nbr1[i] < nbr2[i])
+                {
+                    nbr2[i - 1]++;
+                    nbr1[i] += 2;
+                }
+                arrayRes[i] = (ushort)(nbr1[i] - nbr2[i]);
+            }
+
+            if(nbr1[0] >= nbr2[0])
+            {
+                arrayRes[0] = (ushort)(nbr1[0] - nbr2[0]);
+            }
+            else
+            {
+                ok = false;
+            }
+            binaire = arrayRes;
+
+            return ok;
+
+
+        }
+
+        private void arr(ushort[] array)
+        {
+            Afficheur.Text += "     ";
+            for(int i = 0; i < array.Length; i++)
+            {
+                Afficheur.Text += array[i];
+            }
         }
 
         private string Soustraction(string nbr1, string nbr2)
@@ -134,6 +245,23 @@ namespace CalculetteBinaire
             double.TryParse(value, out nbr);
 
             return nbr;
+        }
+
+        private ushort[] FillArray(string binaire)
+        {
+            ushort[] array = new ushort[8];
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = 0;
+            }
+            //Afficheur.Text = binaire.Length.ToString();
+            for (int i = 0; i < binaire.Length; i++)
+            {
+                array[7 - i] = ushort.Parse(binaire[binaire.Length - 1 - i].ToString());
+            }
+
+            return array;
         }
     }
 }
