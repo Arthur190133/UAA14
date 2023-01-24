90867f04145ddc1e13d8769f3628a9875f7e12e5
Ryckbosch Arthur <190133@CCM-Asty-Moulin.local>
2023-01-24 13:37:22 +0100

    ajout interro

---
+++ POO/I623_Arthur/I623_Arthur/PaintBallGun.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace I623_Arthur
+{
+    class PaintBallGun
+    {
+        private int _bulletPocket = 0;
+        private int _bulletMag = 0;
+        private int _magSize = 16;
+
+        public int BulletPocket
+        {
+            get
+            {
+                return _bulletPocket;
+            }
+            set
+            {
+                _bulletPocket = value;
+            }
+        }
+
+        public int BulletMag
+        {
+            get
+            {
+                return _bulletMag;
+            }
+            set
+            {
+                _bulletMag = value;
+            }
+        }
+
+
+        public int MagSize
+        {
+            get
+            {
+                return _magSize;
+            }
+        }
+
+        public PaintBallGun(int Pocket, int Mag)
+        {
+            _bulletPocket = Pocket;
+            _bulletMag = Mag;
+        }
+
+        public bool IsEmpty() {return (_bulletMag <= 0);}
+
+        public bool Fire()
+        {
+            if (IsEmpty())
+            {
+                return false;
+            }
+            else
+            {
+                _bulletMag--;
+                return true;
+            }
+        }
+
+        public bool Reload()
+        {
+            if(_bulletMag == _magSize || _bulletPocket <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                int oldBulletMag = _bulletMag;
+                _bulletMag = (_bulletPocket >= _magSize) ? _magSize : _bulletPocket + _bulletMag;
+                _bulletPocket -= (_bulletMag - oldBulletMag);
+                return true;
+            }
+        }
+
+        public void TakeBullets()
+        {
+            _bulletPocket += 30;
+        }
+
+        // Question 3
+        private void ChangeMagSize(int newMagSize)
+        {
+            _magSize = newMagSize;
+        }
+    }
+}
---
+++ POO/I623_Arthur/I623_Arthur/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace I623_Arthur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaintBallGun gun = new PaintBallGun(30, 0);
+            string choice = "";
+            Console.WriteLine("Bienvenue dans ce jeu de tir ... Vous démarrez avec 30 balles");
+            Console.WriteLine("=============================================================");
+            do
+            {
+                if(choice == "r")
+                {
+                    int oldBullets = gun.BulletMag;
+                    if (gun.Reload())
+                    {
+                        Console.WriteLine("=> Recharge de " + (gun.BulletMag - oldBullets) + " balles dans le chargeur effectuée");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Impossible de recharger !");
+                    }
+                   
+                }
+                else if(choice == "+")
+                {
+                    gun.TakeBullets();
+                    Console.WriteLine("Recharge de 30 balles effectuée dans votre poche");
+                }
+                else if(choice == " ")
+                {
+                    if (gun.Fire())
+                    {
+                        Console.WriteLine("=> Tir effectué !");
+                    }
+                    else
+                    {
+                        Console.WriteLine("=> Impossible de tirer !");
+                    }
+                    
+                }
+
+                Console.WriteLine("Vous avez un total de " + gun.BulletPocket + " dans votre poche et " + gun.BulletMag + " balles dans le chargeur");
+                if (gun.IsEmpty())
+                {
+                    Console.WriteLine("Attention votre chargeur est vide");
+                }
+                Console.WriteLine("Espace pour tirer \nr pour recharger, \n+ pour reprendre des munitions, \nq pour quitter");
+                choice = Console.ReadLine();
+            } while (choice != "q");
+        }
+    }
+}
