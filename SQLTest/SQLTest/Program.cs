3187f050fd6d835346007e805b0a4f982e62f483
Ryckbosch Arthur <190133@CCM-Asty-Moulin.local>
2023-02-17 14:18:04 +0100

    ajout sql test

---
+++ SQLTest/SQLTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace sqltest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            selectBookByTitle("Kaimiloa");
+            Console.ReadLine();
+        }
+
+        static string getDatabasePath() { return "Data Source = PCI42106\\SQLEXPRESS; Initial Catalog = bibliotheque_bb; Integrated Security = True"; }
+
+        static void selectBookByTitle(string title)
+        {
+         
+           string query = string.Format("SELECT * from listeLivre where titre = @title", title);
+
+            SqlConnection connexion = new SqlConnection(getDatabasePath());
+            SqlCommand command = new SqlCommand(query, connexion);
+            connexion.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                
+                Console.WriteLine(reader[3]);
+            }
+
+
+        }
+    }
+}
